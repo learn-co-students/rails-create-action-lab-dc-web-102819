b4f9713f02db16764d756a2325b00c6caea452f5
jdromero88 <github email address>
2019-11-24 18:27:42 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,17 +4,23 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
-  def show
-    @student = Student.find(params[:id])
-  end
-
   def new
+    @student = Student.new
   end
 
   def create
-    
-    student.create()
-    redirect_to student_link(student)
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save
+    # student = Student.create(first_name: first_name, last_name: last_name)
+    # student = Student.create(first_name: first_name, last_name: last_name)
+    # student = Student.create(first_name: params[:first_name],
+      # last_name: params[:last_name])
+    redirect_to student_path(@student)
+  end
+  def show
+    @student = Student.find(params[:id])
   end
 
 end
---
+++ db/migrate/20191124033252_change_columns.rb
@@ -0,0 +1,3 @@
+class ChangeColumns < ActiveRecord::Migration[5.0]
+
+end
